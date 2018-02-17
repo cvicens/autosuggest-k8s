b919bb115f3aca29d9ddc7c19a2b1ffe8450a958
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-02-17 21:00:17 +0100

    fix

--- create-clusters.sh
+++ create-clusters.sh
@@ -1,26 +1,9 @@
 #!/bin/sh
 
-# Generic environment
-export PROJECT_ID="autosuggest-194816"
+# Environment
+. ./env.sh
 
-export NETWORK_NAME="autosuggest-network"
-
-export MACHINE_TYPE="n1-standard-1"
-export NUM_NODES="2"
-export DISK_SIZE="40"
-
-# Specific environment
-export EU_CLUSTER_NAME="autosuggest-cluster-eu"
-export EU_COMPUTE_REGION="europe-west1"
-export EU_COMPUTE_ZONE="${EU_COMPUTE_REGION}-d"
-export EU_SUBNETWORK_NAME="autosuggest-subnetwork-${EU_COMPUTE_ZONE}"
-
-export US_CLUSTER_NAME="autosuggest-cluster-us"
-export US_COMPUTE_REGION="us-central1"
-export US_COMPUTE_ZONE="${US_COMPUTE_REGION}-a"
-export US_SUBNETWORK_NAME="autosuggest-subnetwork-${US_COMPUTE_ZONE}"
-
-# Custom VPV network
+# Custom VPC network
 gcloud compute networks create ${NETWORK_NAME} \
     --subnet-mode custom
 
@@ -41,7 +24,7 @@ gcloud container clusters create $EU_CLUSTER_NAME \
   --disk-size=$DISK_SIZE \
   --zone=${EU_COMPUTE_ZONE} \
   --network=${NETWORK_NAME} \
-  --subnetwork=${SUBNETWORK_NAME}
+  --subnetwork=${EU_SUBNETWORK_NAME}
 
 # Create cluster onUS
 gcloud container clusters create $US_CLUSTER_NAME \
@@ -50,5 +33,5 @@ gcloud container clusters create $US_CLUSTER_NAME \
   --disk-size=$DISK_SIZE \
   --zone=${US_COMPUTE_ZONE} \
   --network=${NETWORK_NAME} \
-  --subnetwork=${SUBNETWORK_NAME}
+  --subnetwork=${US_SUBNETWORK_NAME}
 
