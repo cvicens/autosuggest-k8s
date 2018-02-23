0d8f5d227d4d5807f9a0e61f3bc827f9ad8a7115
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-02-23 11:53:00 +0100

    new

--- dashboard.sh
+++ dashboard.sh
@@ -10,12 +10,15 @@ fi
 
 if [ "$1" = "EU" ]; then
     gcloud container clusters get-credentials ${EU_CLUSTER_NAME} --zone ${EU_COMPUTE_ZONE} --project ${PROJECT_ID}
-    kubectl proxy --port=0
-    exit 0 
 fi
 
 if [ "$1" = "US" ]; then
     gcloud container clusters get-credentials ${US_CLUSTER_NAME} --zone ${US_COMPUTE_ZONE} --project ${PROJECT_ID}
-    kubectl proxy --port=0
-    exit 0 
 fi
+
+kubectl create -f ./dashboard.yaml
+
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+
+kubectl proxy --port=0
+exit 0 
