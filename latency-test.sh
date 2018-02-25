46ece90274bf4dfa9ea669872edccb8020839b29
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2018-02-25 22:03:01 +0100

    latency tests

--- latency-test.sh
+++ latency-test.sh
@@ -5,20 +5,21 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-CONCURRENCY=50
-REQUESTS=5000
+CONCURRENCY=10
+REQUESTS=1000
 REGION=$1
 TERM=$2
 
 if [ "$1" = "EU" ]; then
     ab -n ${REQUESTS} -c ${CONCURRENCY} http://eu.cache.apps.viralmar.es:8080/cache/products/${TERM}
-    exit 0 
 fi
 
 if [ "$1" = "US" ]; then
     ab -n ${REQUESTS} -c ${CONCURRENCY} http://us.cache.apps.viralmar.es:8080/cache/products/${TERM}
-    exit 0 
 fi
 
+ab -n ${REQUESTS} -c ${CONCURRENCY} http://cdn.cache.apps.viralmar.es/cache/products/${TERM}
+
+exit 0 
 
 
